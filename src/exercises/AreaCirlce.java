97af01f20aa5e934ebaa6692601dacdd3dd6d881
Garon Power <garonpower@yahoo.com>
2019-03-05 02:36:14 +0000

    completed bonus

--- src/exercises/AreaCirlce.java
+++ src/exercises/AreaCirlce.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class AreaCirlce {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int radius;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the radius: ");
-        double radius = scanner.nextDouble();
+        do {
+            System.out.println("Enter the radius: ");
+            radius = keyboard.nextInt();
+            if(radius <= 0) {
+                System.out.println("Error: Enter a number greater than zero!");
+            }
+        }while (radius <= 0);
 
         double area = Math.PI * (radius * radius);
         System.out.println("The area = " +area);
--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the radius: ");
-        double radius = scanner.nextDouble();
+        Scanner keyboard = new Scanner(System.in);
+        int radius;
+
+        do {
+            System.out.println("Enter the radius: ");
+            radius = keyboard.nextInt();
+            if(radius <= 0) {
+                System.out.println("Error: Enter a number greater than zero!");
+            }
+        }while (radius <= 0);
 
         double area = Math.PI * (radius * radius);
         System.out.println("The area = " +area);
+
     }
 }
