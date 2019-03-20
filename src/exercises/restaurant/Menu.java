fa4ca3a7e879449556e8ff2af75b8a12ddce5c6a
Garon Power <garonpower@yahoo.com>
2019-03-20 16:27:10 +0000

    added getters & setters

--- src/exercises/restaurant/Menu.java
+++ src/exercises/restaurant/Menu.java
@@ -1,4 +1,19 @@
 package exercises.restaurant;
 
+import java.util.Date;
+
 public class Menu {
+    private Date lastUpdated;
+
+    public Menu(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
 }
--- src/exercises/restaurant/MenuItem.java
+++ src/exercises/restaurant/MenuItem.java
@@ -1,4 +1,60 @@
 package exercises.restaurant;
 
+import java.util.Date;
+
 public class MenuItem {
+
+    private String itemName;
+    private String description;
+    private String category;
+    private int price;
+    private Date dateAdded;
+
+    public MenuItem(String itemName, String description, String category, int price, Date dateAdded) {
+        this.itemName = itemName;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.dateAdded = dateAdded;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
 }
